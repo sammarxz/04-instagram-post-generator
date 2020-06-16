fd3eb9fe15a6eff342d63d687b6cdb35011dec1b
Sam Marxz <sammarxzz@gmail.com>
2020-06-16 13:27:01 -0300

    Generate an image for each quote

--- app.py
+++ app.py
@@ -1,39 +1,71 @@
+import datetime
+import textwrap
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
-
-# from PIL import Image, ImageDraw, ImageFont
-
-# img = Image.new('RGB', (1080, 1080), color = 'black')
-
-# fnt = ImageFont.truetype('./fonts/AUTHENTICSans-60.otf', 22)
-
-# d = ImageDraw.Draw(img)
-# d.text((200, 200), "Hello World", font=fnt, fill=(255,255,255))
-
-# img.save('test.png')
+from PIL import Image, ImageDraw, ImageFont
 
 
-url = 'https://www.codeofliving.com/blog/55-powerful-short-quotes-sayings-life/'
-response = requests.get(url)
+def get_quotes():
+    url = 'https://www.codeofliving.com/blog/55-powerful-short-quotes-sayings-life/'
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    results = soup.find_all('div', class_='exp-smart-read')
+
+    quotes = []
+
+    for item in results:
+        quote_list = item.find('ol')
+
+        if quote_list:
+            for item in quote_list:
+                if not item.findChildren('img', recursive=False):
+                    quote_text = item.text.split('–')[0]
+                    quote_author = item.find('strong').text.strip()
+
+                    quote = {'text': quote_text, 'author': quote_author}
+                    quotes.append(quote)
+
+    return quotes
 
 
-soup = BeautifulSoup(response.text, 'html.parser')
-results = soup.find_all('div', class_='exp-smart-read')
+def draw_multiple_line_text(image, text, fontsize, text_color, text_start_height):
+    font = ImageFont.truetype('./fonts/AUTHENTICSans-60.otf', fontsize)
+    draw = ImageDraw.Draw(image)
+    image_width, image_height = image.size
+    y_text = text_start_height
+    lines = textwrap.wrap(text, width=24)
+    for line in lines:
+        line_width, line_height = font.getsize(line)
+        draw.text((80, y_text), line, font=font, fill=text_color)
+        y_text += line_height * 1.2
 
-quotes = []
 
-for item in results:
-    quote_list = item.find('ol')
+def generate_images(quotes):
+    for i, quote in enumerate(quotes):
+        post_width, post_height = 1080, 1080
 
-    for quote in quote_list:
-        if not quote.findChildren('img', recursive=False):
-            quote_text = quote.text.split('–')[0]
-            quote_author = quote.find('strong').text.strip()
+        image = Image.new('RGB', (post_width, post_height), color='black')
 
-            quote = {'quote': quote_text, 'author': quote_author}
-            quotes.append(quote)
+        fontsize = 72
 
-    print(quotes)
+        text1 = quote['text']
+        text2 = quote['author']
 
+        text_color = (200, 200, 200)
+        author_color = (100, 100, 100)
+
+        text_start_height = 80
+
+        draw_multiple_line_text(image, text1, fontsize, text_color,
+                                text_start_height)
+        draw_multiple_line_text(image, text2, int(fontsize / 2), author_color, 
+                                post_height - text_start_height * 1.4)
+
+        filename = '{}.png'.format(i)
+        image.save('./posts/{}'.format(filename))
+
+
+if __name__ == '__main__':
+    quotes = get_quotes()
+    generate_images(quotes)
