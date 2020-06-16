3fb3031e85efab43965b6ec4c05e8ad619bf1a20
Sam Marxz <sammarxzz@gmail.com>
2020-06-16 11:38:16 -0300

    Test scrapper

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from PIL import Image, ImageDraw, ImageFont
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
 
-img = Image.new('RGB', (1080, 1080), color = 'black')
+# from PIL import Image, ImageDraw, ImageFont
 
-fnt = ImageFont.truetype('./fonts/AUTHENTICSans-60.otf', 22)
+# img = Image.new('RGB', (1080, 1080), color = 'black')
 
-d = ImageDraw.Draw(img)
-d.text((200, 200), "Hello World", font=fnt, fill=(255,255,255))
+# fnt = ImageFont.truetype('./fonts/AUTHENTICSans-60.otf', 22)
 
-img.save('test.png')
+# d = ImageDraw.Draw(img)
+# d.text((200, 200), "Hello World", font=fnt, fill=(255,255,255))
+
+# img.save('test.png')
+
+
+url = 'https://www.codeofliving.com/blog/55-powerful-short-quotes-sayings-life/'
+response = requests.get(url)
+
+
+soup = BeautifulSoup(response.text, 'html.parser')
+results = soup.find_all('div', class_='exp-smart-read')
+
+quotes = []
+
+for item in results:
+    quote_list = item.find('ol')
+
+    for quote in quote_list:
+        if not quote.findChildren('img', recursive=False):
+            quote_text = quote.text.split('–')[0]
+            quote_author = quote.find('strong').text.strip()
+
+            quote = {'quote': quote_text, 'author': quote_author}
+            quotes.append(quote)
+
+    print(quotes)
+
